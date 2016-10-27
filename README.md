105f6683c3f145dade60c81ac3ff67003330d8a4
Jérémy Buget <jbuget@octo.com>
2016-10-27 14:08:50 +0200

    List boxes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # poc-node-imap
 Proof of concept that it is possible to fetch &amp; parse notification emails (sent by Facebook, Twitter, etc.) from a Gmail account
+
+- [node-imap](https://github.com/mscdex/node-imap)
+- [mailparser](https://github.com/andris9/mailparser)
+- https://github.com/chirag04/mail-listener2
--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const Imap = require('imap');
 const inspect = require('util').inspect;
 
 const imap = new Imap({
-  debug: (debugInfo) => {
-    console.log(debugInfo);
-  },
   user: 'poc.node.imap@gmail.com',
   password: 'imap.node.poc',
   host: 'imap.gmail.com',
@@ -16,55 +13,77 @@ function openInbox(cb) {
   imap.openBox('INBOX', true, cb);
 }
 
-imap.once('ready', function () {
-  openInbox(function (err, box) {
+imap.once('ready', _ => {
 
-    if (err) throw err;
-
-    const imapFetch = imap.seq.fetch('1:3', {
-      bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
-      struct: true
-    });
-
-    imapFetch.on('message', function (imapMessage, seqno) {
-      console.log('Message #%d', seqno);
-      const prefix = '(#' + seqno + ') ';
-
-      imapMessage.on('body', function (stream, info) {
-        let buffer = '';
-        stream.on('data', function (chunk) {
-          buffer += chunk.toString('utf8');
-        });
-        stream.once('end', function () {
-          console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
-        });
-      });
-
-      imapMessage.once('attributes', function (attrs) {
-        console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
-      });
-
-      imapMessage.once('end', function () {
-        console.log(prefix + 'Finished');
-      });
-    });
-
-    imapFetch.once('error', function (err) {
-      console.log('Fetch error: ' + err);
-    });
-
-    imapFetch.once('end', function () {
-      console.log('Done fetching all messages!');
-      imap.end();
-    });
+  imap.getBoxes((err, boxes) => {
+    console.log('\n----- Imap#getBoxes -----\n');
+    console.log(inspect(boxes));
+    console.log('\n-------------------------\n');
   });
+
+  imap.subscribeBox('INBOX', err => {
+    console.log('\n----- Imap#subscribeBox -----\n');
+    if (err) throw err;
+    console.log('Listening for mails...');
+    console.log('\n-------------------------\n');
+  });
+
+  /*
+    openInbox((err, box) => {
+
+      if (err) throw err;
+
+      const imapFetch = imap.seq.fetch('1:*', {
+        bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
+        struct: true
+      });
+
+      imapFetch.on('message', (imapMessage, seqno) => {
+
+        console.log('Message #%d', seqno);
+        const prefix = '(#' + seqno + ') ';
+
+        imapMessage.on('body', (stream, info) => {
+          let buffer = '';
+          stream.on('data', chunk => {
+            buffer += chunk.toString('utf8');
+          });
+          stream.once('end', _ => {
+            console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+          });
+        });
+
+        imapMessage.once('attributes', attrs => {
+          console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+        });
+
+        imapMessage.once('end', _ => {
+          console.log(prefix + 'Finished');
+        });
+      });
+
+      imapFetch.once('error', err => {
+        console.log('Fetch error: ' + err);
+      });
+
+      imapFetch.once('end', _ => {
+        console.log('Done fetching all messages!');
+        imap.end();
+      });
+    });
+  */
 });
 
-imap.once('error', function (err) {
+imap.on('mail', numNewMsgs => {
+
+  console.log('New message has come : ' + numNewMsgs);
+});
+
+imap.once('error', err => {
   console.log(err);
 });
 
-imap.once('end', function () {
+imap.once('end', _ => {
   console.log('Connection ended');
 });
 
